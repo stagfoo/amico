3b743c7d592ec61b2adbf40fa4ee9515cfbc1214
Alex King <alex@basal.dev>
2021-12-24 09:53:29 +0900

    💽　assets ░ change annoying video

--- frontend/src/video/index.ts
+++ frontend/src/video/index.ts
@@ -1,6 +1,6 @@
 import dashjs from 'dashjs';
 export function loadDashVideo(){
-    let url = "https://dash.akamaized.net/envivio/EnvivioDash3/manifest.mpd";
+    let url = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
     var player = dashjs.MediaPlayer().create();
     player.initialize((document.querySelector("#dash") as any), url, true);
 }
